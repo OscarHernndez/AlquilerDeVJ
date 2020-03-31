fc64ad053d012f3269ebcbdf8e664b7da2022fe2
oscarhernandez <oscar1001996@live.com>
2020-03-31 00:07:19 -0600

    Aplicando un template para el sitio web público

---
+++ AlquilerVJ/AlquilerVJ.Web/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using AlquilerVJ.BL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AlquilerVJ.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductosActivos();
+
+            ViewBag.adminWebsiteUrl = 
+                ConfigurationManager.AppSettings["adminWebsiteUrl"]; //Me permite leer el valor que esta en Web.Config de .Web y CM-me permite leer el appSettings
+
+            return View(listadeProductos);
+        }
+    }
+}
